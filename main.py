b0376948eb7c976958d5cd21873d50938fb4fe3e
Abdel <abdelkader.habel_pro@outlook.com>
2023-01-27 16:55:20 -0500

    initial commit abdelkader

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import django
 if __name__ == '__main__':
     print(django.get_version())
     print('second commit')
+    print('abdelkader was here')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
