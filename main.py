7fa58eaa4ba11f80acf754924dd219a38711d63c
HakimSkaik <98859282+HakimSkaik@users.noreply.github.com>
2023-02-02 23:49:07 -0500

    Update main.py
    Removed extra space

--- main.py
+++ main.py
@@ -7,6 +7,5 @@ if __name__ == '__main__':
     print('abdelkader was here')
 
     print('Zakaria was here! Goat Summer 2023 *fire emoji*')
-    print('test link commit nikzebi with issue')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
