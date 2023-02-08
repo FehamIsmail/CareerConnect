bbc53840cd48fa4053b26737e57f90159fd202e4
ismail <ismail.feham64@gmail.com>
2023-02-07 23:36:53 -0500

    added react-typescript frontend component

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import React, {FC} from 'react';
+
+
+const App:FC = () => {
+    return(
+      <StyledDiv></StyledDiv>
+    )
+}
+
+const StyledDiv = styled.div`
+  width: 200px;
+`
+
+export default App;
