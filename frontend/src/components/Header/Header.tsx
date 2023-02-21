41e18a7f9abee3eb782c57a0a5fda17831a3c19c
ismail <ismail.feham64@gmail.com>
2023-02-20 22:44:35 -0500

    setting up TailwindCSS on React closes #30

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,274 @@
+import {FC, Fragment} from 'react'
+import React from 'react';
+import {Menu, Popover, Transition} from '@headlessui/react'
+import {
+    BriefcaseIcon,
+    ClipboardDocumentIcon,
+    Bars3Icon,
+    BookmarkSquareIcon,
+    CalendarIcon,
+    ChartBarIcon,
+    CursorArrowRaysIcon,
+    LifebuoyIcon,
+    PhoneIcon,
+    PlayIcon,
+    ShieldCheckIcon,
+    XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import {classNames} from "../../scripts/utils";
+import logo from '../../assets/logo_nobg.svg'
+
+const solutions = [
+    {
+        name: 'Job Postings',
+        description: 'Get a better understanding of where your traffic is coming from.',
+        href: '#',
+        icon: ClipboardDocumentIcon,
+    },
+    {
+        name: 'Employers / Post Job',
+        description: 'Speak directly to your customers in a more meaningful way.',
+        href: '#',
+        icon: BriefcaseIcon,
+    },
+]
+
+const resources = [
+    {
+        name: 'Help Center',
+        description: 'Get all of your questions answered in our forums or contact support.',
+        href: '#',
+        icon: LifebuoyIcon,
+    },
+    {
+        name: 'Guides',
+        description: 'Learn how to maximize our platform to get the most out of it.',
+        href: '#',
+        icon: BookmarkSquareIcon,
+    },
+    {
+        name: 'Events',
+        description: 'See what meet-ups and other events we might be planning near you.',
+        href: '#',
+        icon: CalendarIcon,
+    },
+    { name: 'Security', description: 'Understand how we take your privacy seriously.', href: '#', icon: ShieldCheckIcon },
+]
+
+const recentPosts = [
+    { id: 1, name: 'Boost your conversion rate', href: '#' },
+    { id: 2, name: 'How to use search engine optimization to drive traffic to your site', href: '#' },
+    { id: 3, name: 'Improve your customer experience', href: '#' },
+]
+
+
+
+const Header:FC = () => {
+    return (
+        <Popover className="relative bg-white border-b-2">
+            <div className="mx-auto max-w-screen-xl px-8">
+                <div className="flex items-center justify-between border-gray-100 py-3 md:justify-start md:space-x-10">
+                    <div className="flex justify-start lg:w-0 lg:flex-1  ">
+                        <a href="#" className="flex bg-indigo-600 cursor-pointer px-2 py-3 rounded-lg">
+                            <span className="sr-only">CareerConnect</span>
+                            {/*<embed src={logo} className="h-[1rem] white w-auto sm:h-5 pointer-events-auto embed-logo"/>*/}
+                            <img color="black"
+                                className="h-3 w-auto sm:h-4 svg-white"
+                                src={logo}
+                                alt="Logo"
+                            />
+                        </a>
+                    </div>
+                    <div className="-my-2 -mr-2 md:hidden">
+                        <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                            <span className="sr-only">Open menu</span>
+                            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                        </Popover.Button>
+                    </div>
+                    <Popover.Group as="nav" className="hidden space-x-10 md:flex">
+                        <Popover className="relative">
+                            {({ open }) => (
+                                <>
+                                    <Popover.Button
+                                        className={classNames(
+                                            open ? 'text-gray-900' : 'text-gray-500',
+                                            'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                                        )}
+                                    >
+                                        <span>Jobs</span>
+                                        <ChevronDownIcon
+                                            className={classNames(
+                                                open ? 'text-gray-600' : 'text-gray-400',
+                                                'ml-2 h-5 w-5 group-hover:text-gray-500'
+                                            )}
+                                            aria-hidden="true"
+                                        />
+                                    </Popover.Button>
+
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-200"
+                                        enterFrom="opacity-0 translate-y-1"
+                                        enterTo="opacity-100 translate-y-0"
+                                        leave="transition ease-in duration-150"
+                                        leaveFrom="opacity-100 translate-y-0"
+                                        leaveTo="opacity-0 translate-y-1"
+                                    >
+                                        <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
+                                            <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                                                <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                                                    {solutions.map((item) => (
+                                                        <a
+                                                            key={item.name}
+                                                            href={item.href}
+                                                            className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
+                                                        >
+                                                            <item.icon className="h-6 w-6 flex-shrink-0 text-indigo-600" aria-hidden="true" />
+                                                            <div className="ml-4">
+                                                                <p className="text-base font-medium text-gray-900">{item.name}</p>
+                                                                <p className="mt-1 text-sm text-gray-500">{item.description}</p>
+                                                            </div>
+                                                        </a>
+                                                    ))}
+                                                </div>
+                                            </div>
+                                        </Popover.Panel>
+                                    </Transition>
+                                </>
+                            )}
+                        </Popover>
+                    </Popover.Group>
+                    <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+                        <a href="src/components/Header/Header#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                            Sign in
+                        </a>
+                        <a
+                            href="src/components/Header/Header#"
+                            className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                        >
+                            Sign up
+                        </a>
+                        <Menu as="div" className="relative ml-3">
+                            <div>
+                                <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                    <span className="sr-only">Open user menu</span>
+                                    <img
+                                        className="h-8 w-8 rounded-full"
+                                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                        alt=""
+                                    />
+                                </Menu.Button>
+                            </div>
+                            <Transition
+                                as={Fragment}
+                                enter="transition ease-out duration-100"
+                                enterFrom="transform opacity-0 scale-95"
+                                enterTo="transform opacity-100 scale-100"
+                                leave="transition ease-in duration-75"
+                                leaveFrom="transform opacity-100 scale-100"
+                                leaveTo="transform opacity-0 scale-95"
+                            >
+                                <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                    <Menu.Item>
+                                        {({ active }) => (
+                                            <a
+                                                href="#"
+                                                className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                            >
+                                                Your Profile
+                                            </a>
+                                        )}
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        {({ active }) => (
+                                            <a
+                                                href="#"
+                                                className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                            >
+                                                Settings
+                                            </a>
+                                        )}
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        {({ active }) => (
+                                            <a
+                                                href="#"
+                                                className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                            >
+                                                Sign out
+                                            </a>
+                                        )}
+                                    </Menu.Item>
+                                </Menu.Items>
+                            </Transition>
+                        </Menu>
+                    </div>
+                </div>
+            </div>
+            <Transition
+                as={Fragment}
+                enter="duration-200 ease-out"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="duration-100 ease-in"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+            >
+                <Popover.Panel focus className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
+                    <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                        <div className="px-5 pt-5 pb-6">
+                            <div className="flex items-center justify-between">
+                                <div>
+                                    <img
+                                        className="h-8 w-auto"
+                                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                        alt="Your Company"
+                                    />
+                                </div>
+                                <div className="-mr-2">
+                                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                        <span className="sr-only">Close menu</span>
+                                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                                    </Popover.Button>
+                                </div>
+                            </div>
+                            <div className="mt-6">
+                                <nav className="grid gap-y-8">
+                                    {solutions.map((item) => (
+                                        <a
+                                            key={item.name}
+                                            href={item.href}
+                                            className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50"
+                                        >
+                                            <item.icon className="h-6 w-6 flex-shrink-0 text-indigo-600" aria-hidden="true" />
+                                            <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
+                                        </a>
+                                    ))}
+                                </nav>
+                            </div>
+                        </div>
+                        <div className="space-y-6 py-6 px-5">
+                            <div>
+                                <a
+                                    href="src/components/Header/Header#"
+                                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                                >
+                                    Sign up
+                                </a>
+                                <p className="mt-6 text-center text-base font-medium text-gray-500">
+                                    Existing customer?{' '}
+                                    <a href="src/components/Header/Header#" className="text-indigo-600 hover:text-indigo-500">
+                                        Sign in
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </Popover.Panel>
+            </Transition>
+        </Popover>
+    )
+}
+
+export default Header;
