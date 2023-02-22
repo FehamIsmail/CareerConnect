d4c81914faceac6db73a8501b6c2b55b947ac897
HakimSkaik <98859282+HakimSkaik@users.noreply.github.com>
2023-02-21 22:25:46 -0500

    Added home landing page
    Displayed home landing image and added a search bar with a search icon

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Header/Header';
+import Home from '../Home/Home';
 
 const App = () => {
     return(
         <>
           <Header isStudent={true} loggedIn={false} />
+          <Home/>
         </>
     )
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+import './input.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
