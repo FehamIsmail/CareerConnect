e53350ffbcdd632e2e469cb53e2cf339e61283ab
ismail <ismail.feham64@gmail.com>
2023-03-01 19:26:08 -0500

    Setting react router dom closes #54

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+import {BrowserRouter} from "react-router-dom";
+import {RecoilRoot} from "recoil";
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <RecoilRoot>
+                <App/>
+            </RecoilRoot>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
