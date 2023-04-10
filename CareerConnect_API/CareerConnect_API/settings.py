5e98b4def92cab42e172976f294b4ca3fe3e8557
Abdel <abdelkader.habel_pro@outlook.com>
2023-04-10 16:11:51 -0400

    Students receive an email when the status of their application changes. close #133

--- CareerConnect_API/CareerConnect_API/settings.py
+++ CareerConnect_API/CareerConnect_API/settings.py
@@ -182,3 +182,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "api.User"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'careerconnectupdates@gmail.com'
+EMAIL_HOST_PASSWORD = 'tjkqjfsocmroxjbh'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
