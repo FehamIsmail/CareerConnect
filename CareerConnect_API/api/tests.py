80581fa473e6e0d0f247cd2a91da1309945e87f5
Abdel <abdelkader.habel_pro@outlook.com>
2023-04-17 04:42:21 -0400

    wrote unit test for CoverLetterListView #163

--- CareerConnect_API/api/tests.py
+++ CareerConnect_API/api/tests.py
@@ -2,8 +2,8 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import User, Role, StudentProfile, EmployerProfile, CurriculumVitae
-from .serializers import UserSerializer, StudentProfileSerializer, EmployerProfileSerializer, CVSerializer
+from .models import User, Role, StudentProfile, EmployerProfile, CurriculumVitae,CoverLetter
+from .serializers import UserSerializer, StudentProfileSerializer, EmployerProfileSerializer
 from oauth2_provider.models import Application, AccessToken
 from django.utils import timezone
 from datetime import datetime,timedelta
@@ -148,7 +148,7 @@ class EmployerProfileViewTestCase(APITestCase):
         self.assertEqual(self.user.email, updated_data['email'])
         self.assertEqual(self.employer_profile.company, updated_data['company'])
 
-
+#
 class CurriculumVitaeListViewTestCase(APITestCase):
 
     def setUp(self):
@@ -260,7 +260,63 @@ class CurriculumVitaeDetailViewTest(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token.token)
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print(response.data)
         self.assertEqual(response.data['title'], CurriculumVitae.objects.first().title)
         self.assertEqual(response.data['default'],CurriculumVitae.objects.first().default)
         self.assertEqual(response.data['id'], str(CurriculumVitae.objects.first().id))
+
+
+class CoverLetterListViewTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(email='userprofile@example.com', password='testpassword',
+                                             role=Role.STUDENT)
+        # get the student's profile
+        self.student_profile = StudentProfile.objects.get(user=self.user)
+        self.client.force_authenticate(user=self.user)
+        self.url = reverse('cl-list')
+
+    def test_create_cl(self):
+        cl_file1 = SimpleUploadedFile("cl.pdf", b"test cl", content_type="application/pdf")
+        cl_info1 = {
+            'cover_letter': cl_file1,
+            'title': "TEST CL"
+        }
+
+        response = self.client.post(self.url, data=cl_info1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.assertEqual((CoverLetter.objects.count()), 1)
+
+        # check the two cvs are saved correctly
+        cl1 = CoverLetter.objects.last()
+
+        self.assertEqual(cl1.title, cl_info1['title'])
+        self.assertEqual(cl1.student_profile, self.student_profile)
+
+    def test_list_cl(self):
+        # create 2 cvs to be able to test the list function
+        CoverLetter.objects.create(
+            student_profile=self.student_profile,
+            title='CL 1',
+            default=True
+        )
+        CoverLetter.objects.create(
+            student_profile=self.student_profile,
+            title='CL 2',
+            default=False
+        )
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+        self.assertEqual(response.data[0]['title'], 'CL 1')
+        self.assertEqual(response.data[0]['default'], True)
+
+        self.assertEqual(response.data[1]['title'], 'CL 2')
+        self.assertEqual(response.data[1]['default'], False)
+
+        # check that the student profile is not displayed
+        self.assertNotIn('student_profile', response.data[0])
+        self.assertIn('cover_letter', response.data[0])
