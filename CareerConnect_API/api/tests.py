077291f0f982bfd4171064ff025c2afa1b033c6b
Abdel <abdelkader.habel_pro@outlook.com>
2023-04-17 13:42:15 -0400

    wrote unit test for JobListView #164

--- CareerConnect_API/api/tests.py
+++ CareerConnect_API/api/tests.py
@@ -2,7 +2,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import User, Role, StudentProfile, EmployerProfile, CurriculumVitae, CoverLetter, ApplicationPackage
+from .models import User, Role, StudentProfile, EmployerProfile, CurriculumVitae, CoverLetter, ApplicationPackage, Job
 from .serializers import UserSerializer, StudentProfileSerializer, EmployerProfileSerializer
 from oauth2_provider.models import Application, AccessToken
 from django.utils import timezone
@@ -409,6 +409,7 @@ class ApplicationPackageListViewTestCase(APITestCase):
         self.assertEqual(package.default, data['default'])
         self.assertEqual(package.student_profile, self.student_profile)
 
+
 # def test_update_application_package(self):
 #     self.client.force_authenticate(user=self.student_profile.user)
 #
@@ -480,3 +481,36 @@ class ApplicationPackageDetailViewTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], 'Updated Package')
         self.assertEqual(response.data['default'], True)
+
+
+class JobListViewTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email='employer@employer.com', password='test123', role=Role.EMPLOYER)
+        self.employer_profile = EmployerProfile.objects.create(user=self.user,company='genetec')
+        # self.job = Job.objects.create(employer_profile=self.employer_profile, title="Test job posting",
+        #                               short_description="short description of the job")
+        self.url = reverse('jobs-list')
+
+    def test_list_jobs(self):
+        employer2=User.objects.create_user(email='employer2@employer.com',password='iukcve',role=Role.EMPLOYER)
+        profile2=EmployerProfile.objects.create(user=employer2,company='ubisoft')
+
+        Job.objects.create(employer_profile=self.employer_profile, title="Job1",
+                           short_description="short description of the job")
+        Job.objects.create(employer_profile=profile2, title="Job2",
+                           short_description="short description of the job")
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+        self.assertEqual(response.data[0]['title'], 'Job1')
+        self.assertEqual(response.data[0]['short_description'], "short description of the job")
+        self.assertEqual(response.data[0]['employer_profile']['company'],self.employer_profile.company)
+
+        self.assertEqual(response.data[1]['title'], 'Job2')
+        self.assertEqual(response.data[1]['short_description'], "short description of the job")
+        self.assertEqual(response.data[1]['employer_profile']['company'],profile2.company)
+
+        self.assertNotIn('application_packages', response.data[0])
