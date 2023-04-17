0c29996efb8657d7ff822a72aa0d8b335b8dee75
Abdel <abdelkader.habel_pro@outlook.com>
2023-04-17 04:48:04 -0400

    wrote unit test for CoverLetterDetailView closes #163

--- CareerConnect_API/api/tests.py
+++ CareerConnect_API/api/tests.py
@@ -2,11 +2,12 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import User, Role, StudentProfile, EmployerProfile, CurriculumVitae,CoverLetter
+from .models import User, Role, StudentProfile, EmployerProfile, CurriculumVitae, CoverLetter
 from .serializers import UserSerializer, StudentProfileSerializer, EmployerProfileSerializer
 from oauth2_provider.models import Application, AccessToken
 from django.utils import timezone
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
+
 
 class RegistrationViewTestCase(APITestCase):
     def test_register_student(self):
@@ -148,6 +149,7 @@ class EmployerProfileViewTestCase(APITestCase):
         self.assertEqual(self.user.email, updated_data['email'])
         self.assertEqual(self.employer_profile.company, updated_data['company'])
 
+
 #
 class CurriculumVitaeListViewTestCase(APITestCase):
 
@@ -208,7 +210,6 @@ class CurriculumVitaeListViewTestCase(APITestCase):
 
 class CurriculumVitaeDetailViewTest(APITestCase):
     def setUp(self):
-
         self.user = User.objects.create_user(email='userprofile@example.com', password='testpassword',
                                              role=Role.STUDENT)
 
@@ -261,7 +262,7 @@ class CurriculumVitaeDetailViewTest(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], CurriculumVitae.objects.first().title)
-        self.assertEqual(response.data['default'],CurriculumVitae.objects.first().default)
+        self.assertEqual(response.data['default'], CurriculumVitae.objects.first().default)
         self.assertEqual(response.data['id'], str(CurriculumVitae.objects.first().id))
 
 
@@ -320,3 +321,61 @@ class CoverLetterListViewTestCase(APITestCase):
         # check that the student profile is not displayed
         self.assertNotIn('student_profile', response.data[0])
         self.assertIn('cover_letter', response.data[0])
+
+
+class CoverLetterDetailViewTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email='userprofile@example.com', password='testpassword',
+                                             role=Role.STUDENT)
+
+        self.student_profile = StudentProfile.objects.get(
+            user=self.user
+        )
+        self.cl = CoverLetter.objects.create(
+            student_profile=self.student_profile,
+            title='CL Title',
+            default=True,
+            cover_letter=SimpleUploadedFile("cl.pdf", b"test cl", content_type="application/pdf")
+        )
+
+        self.application = Application.objects.create(
+            name='Test Application',
+            user=self.user,
+            authorization_grant_type=Application.GRANT_CLIENT_CREDENTIALS
+        )
+
+        self.access_token = AccessToken.objects.create(
+            user=self.user,
+            token='testtoken',
+            application=self.application,
+            expires=timezone.now() + timedelta(days=1)
+        )
+
+    def test_update_cl(self):
+        url = reverse('cl-detail', kwargs={'pk': self.cl.id})
+        updated_data = {
+            'title': 'Updated Title',
+            'default': False
+        }
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token.token)
+        response = self.client.put(url, data=updated_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.cl.refresh_from_db()
+        self.assertEqual(self.cl.title, updated_data['title'])
+        self.assertEqual(self.cl.default, updated_data['default'])
+
+    def test_delete_cl(self):
+        url = reverse('cl-detail', kwargs={'pk': self.cl.id})
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token.token)
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(CoverLetter.objects.filter(id=self.cl.id).exists())
+
+    def test_get_cl(self):
+        url = reverse('cl-detail', kwargs={'pk': self.cl.id})
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token.token)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], CoverLetter.objects.first().title)
+        self.assertEqual(response.data['default'], CoverLetter.objects.first().default)
+        self.assertEqual(response.data['id'], str(CoverLetter.objects.first().id))
