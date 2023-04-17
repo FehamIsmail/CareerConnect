72e5f92165ccdaad7c3a6a79c7f87365e118680f
Abdel <abdelkader.habel_pro@outlook.com>
2023-04-17 13:08:26 -0400

    wrote unit test for ApplicationPackageListView and ApplicationPackageDetailView #164

--- CareerConnect_API/api/tests.py
+++ CareerConnect_API/api/tests.py
@@ -2,7 +2,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import User, Role, StudentProfile, EmployerProfile, CurriculumVitae, CoverLetter
+from .models import User, Role, StudentProfile, EmployerProfile, CurriculumVitae, CoverLetter, ApplicationPackage
 from .serializers import UserSerializer, StudentProfileSerializer, EmployerProfileSerializer
 from oauth2_provider.models import Application, AccessToken
 from django.utils import timezone
@@ -379,3 +379,104 @@ class CoverLetterDetailViewTest(APITestCase):
         self.assertEqual(response.data['title'], CoverLetter.objects.first().title)
         self.assertEqual(response.data['default'], CoverLetter.objects.first().default)
         self.assertEqual(response.data['id'], str(CoverLetter.objects.first().id))
+
+
+class ApplicationPackageListViewTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email="test@test.com", password='testpassword', role=Role.STUDENT)
+        # self.student_profile = StudentProfile.objects.create(username='testuser', email='testuser@example.com')
+        self.student_profile = StudentProfile.objects.get(user=self.user)
+        self.cv = CurriculumVitae.objects.create(student_profile=self.student_profile)
+        self.cl = CoverLetter.objects.create(student_profile=self.student_profile)
+        self.url = reverse('packages-list')
+
+    def test_create_application_package(self):
+        self.client.force_authenticate(user=self.student_profile.user)
+
+        data = {
+            'title': 'My application package',
+            'cv_id': str(self.cv.id),
+            'cl_id': str(self.cl.id),
+            'default': True,
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        package = ApplicationPackage.objects.get(id=response.data['id'])
+        self.assertEqual(package.title, data['title'])
+        self.assertEqual(package.curriculum_vitae.id, self.cv.id)
+        self.assertEqual(package.cover_letter.id, self.cl.id)
+        self.assertEqual(package.default, data['default'])
+        self.assertEqual(package.student_profile, self.student_profile)
+
+# def test_update_application_package(self):
+#     self.client.force_authenticate(user=self.student_profile.user)
+#
+#     package = ApplicationPackage.objects.create(
+#         student_profile=self.student_profile,
+#         curriculum_vitae=self.cv,
+#         cover_letter=self.cl,
+#         title='My application package',
+#         default=True,
+#     )
+#
+#     new_cv = CurriculumVitae.objects.create(student_profile=self.student_profile)
+#     new_cl = CoverLetter.objects.create(student_profile=self.student_profile)
+#
+#     data = {
+#         'cv_id': str(new_cv.id),
+#         'cl_id': str(new_cl.id),
+#         'title': 'Updated title',
+#         'default': False,
+#     }
+#     response = self.client.put(reverse('package-detail', args=[package.id]), data)
+#     self.assertEqual(response.status_code, status.HTTP_200_OK)
+#
+#     package.refresh_from_db()
+#     self.assertEqual(package.curriculum_vitae.id, new_cv.id)
+#     self.assertEqual(package.cover_letter.id, new_cl.id)
+#     self.assertEqual(package.title, data['title'])
+#     self.assertEqual(package.default, data['default'])
+
+
+class ApplicationPackageDetailViewTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email='testuser@user.com',
+            password='testpass123'
+        )
+        self.student_profile = StudentProfile.objects.get(user=self.user)
+        self.access_token = AccessToken.objects.create(
+            user=self.user,
+            token='testtoken123',
+            application=None,
+            expires=timezone.now() + timedelta(days=1)
+        )
+        self.cv = CurriculumVitae.objects.create(student_profile=self.student_profile)
+        self.cl = CoverLetter.objects.create(student_profile=self.student_profile)
+        self.package = ApplicationPackage.objects.create(
+            student_profile=self.user.student_profile,
+            title='Test Package',
+            default=False,
+            curriculum_vitae=self.cv,
+            cover_letter=self.cl
+
+        )
+
+    def test_get_package_detail(self):
+        url = reverse('package-detail', kwargs={'pk': self.package.pk})
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token.token}')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Test Package')
+
+    def test_update_package_detail(self):
+        url = reverse('package-detail', kwargs={'pk': self.package.pk})
+
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token.token}')
+        response = self.client.put(url, {'title': 'Updated Package', 'default': True, })
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Updated Package')
+        self.assertEqual(response.data['default'], True)
