8803d24e3b5363d3166911207338a872d5b3463e
ismail <ismail.feham64@gmail.com>
2023-04-23 16:26:15 -0400

    add test script to package.json

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,18 +2,13 @@ import { defineConfig } from "cypress";
 
 // @ts-ignore
 export default defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-
   component: {
+    video: false,
     devServer: {
       // @ts-ignore
       framework: "create-react-app",
       bundler: "webpack",
+      baseUrl: "http://localhost:3000",
     },
   },
 });
